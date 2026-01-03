a3de4877282692f4406d0e1527ed480a404e7b07
ekezdu <nogamelton@gmail.com>
2026-01-03 18:07:42 +0100

    Created index.html and java.js. Linked index.html to java.js. Planned out how to due step 1 of project in comments.

---
+++ java.js
@@ -0,0 +1,6 @@
+// create a function named getComputerChoice
+//getComputerChoice returns "rock" "paper" or "scissors"
+
+//use math.random and math.floor to generate numbers 0, 1, or 2
+//if else statements to return rock paper or scissors corresponding with the numbers 
+

9c5ad8be1961a8fbcb68c00835dd4ba6bb26ac09
ekezdu <nogamelton@gmail.com>
2026-01-03 18:09:30 +0100

    Created a function called getComputerChoice as described in step 1 of project. Created variable for the result of the function.

--- java.js
+++ java.js
@@ -4,3 +4,11 @@
 //use math.random and math.floor to generate numbers 0, 1, or 2
 //if else statements to return rock paper or scissors corresponding with the numbers 
 
+
+let result = getComputerChoice(2);
+
+
+function getComputerChoice(max)
+{
+    return Math.floor(Math.random()* max);
+}

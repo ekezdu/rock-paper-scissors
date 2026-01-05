706d43d40a4225dd61463d1725bb8d37f220e76c
ekezdu <nogamelton@gmail.com>
2026-01-05 14:45:47 +0100

    Realized that the if statements were not actually returning anything but printing. Assignment requires the result to be returned. Got stuck. Commented all progress and moved to the bottom of the page for reference. Starting fresh.

--- java.js
+++ java.js
@@ -1,4 +1,3 @@
-//STEP 1
 
 
 // create a function named getComputerChoice
@@ -10,34 +9,47 @@
 
 function getComputerChoice(max)
 {
-    return Math.floor(Math.random()* max);
+    return (Math.floor(Math.random()* max));
+
 }
-
-
 
 
 //Create a prompt for the user to insert their answer rock paper or scissors
 //Prompt is to be called "getHumanChoice" 
 //Assume the user will write a valid choice. 
+// return the value of the prompt
 
 let humanChoicePrompt = prompt("rock, paper, scissors?");
 
 function getHumanChoice()
 {
     humanChoicePrompt;
+    
 }
+console.log(humanChoicePrompt);
+
+let computerChoice = getComputerChoice(3);
 
 
-if (getComputerChoice(3) === 0) 
+if (computerChoice === 0) 
 {
-    console.log("rock")
+    console.log("rock");
+    
 }
-    else if (getComputerChoice(3) === 1)
+    else if (computerChoice === 1)
     {
         console.log("paper");
+        
     }
     else
     {
+        
         console.log("scissors");
     }
 
+console.log(getComputerChoice(3));
+
+// track scores
+//create variables HumanScore and ComputerScore
+//they start with 0
+//check they work by printing in console
